bce4f23dc9d0084e31cfbc99a963ba362964c054
ryanfuchs <44633422+ryanfuchs@users.noreply.github.com>
2020-01-12 16:20:58 +0100

    edited mini-shopping component

--- dorfladen-frontend/src/app/component/mini-shopping-cart/mini-shopping-cart.component.ts
+++ dorfladen-frontend/src/app/component/mini-shopping-cart/mini-shopping-cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from '../../service/product.service';
 import {Product} from '../../types/product.type';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-mini-shopping-cart',
@@ -12,7 +13,7 @@ export class MiniShoppingCartComponent implements OnInit {
   private shoppingCart: Array<Product>;
   public priceSum;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,  private router: Router) { }
 
   async ngOnInit() {
     this.productService.shoppingCartState$.subscribe(async () => {
@@ -34,6 +35,10 @@ export class MiniShoppingCartComponent implements OnInit {
     }
     console.log(sum);
     this.priceSum = sum;
+  }
+
+  async openCart(){
+    await this.router.navigate(['/cart']);
   }
 
 }
