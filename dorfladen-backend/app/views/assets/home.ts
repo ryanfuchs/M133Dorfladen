2e98f73708d4e0c4fa31d8c91ee1f02a56e3dfff
ryanfuchs <44633422+ryanfuchs@users.noreply.github.com>
2020-01-17 18:25:43 +0100

    refactoring

--- dorfladen-backend/app/views/assets/home.ts
+++ dorfladen-backend/app/views/assets/home.ts
@@ -1,11 +1,1 @@
-import { Hero } from "../../types";
-
-const tbody = document.querySelector("table tbody");
-fetch("/api/heroes")
-    .then(r => r.json())
-    .then((heroes: Hero[]) => heroes
-        .forEach(hero => tbody
-            .innerHTML += `
-                <tr>
-                    <td>${hero.name}</td>
-                </tr>`));
+// for backend view (not used now)
